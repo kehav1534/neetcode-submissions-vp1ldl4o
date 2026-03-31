e72884032b57b471a1c6b76c18c13db1925456ce
Keshav Maheshwari <90709089+kehav1534@users.noreply.github.com>
2026-03-31 17:01:36 +0530

    Add: string-compression - submission-6

---
+++ Data Structures & Algorithms/string-compression/submission-6.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int compress(char[] chars) {
+        String compressed = "";
+        int count = 1;
+        for(int i = 0; i < chars.length; i++){
+            if(i==0){ 
+                compressed+=chars[0];
+            }
+            else if(i > 0 && chars[i-1]==chars[i])
+                count++;
+            else if(i > 0 && chars[i-1]!=chars[i]){
+                if(count > 1)
+                    compressed+=count;
+                count = 1;
+                compressed+=chars[i];
+            }
+        }
+        if(count > 1)
+            compressed+=count;
+        for(int i = 1; i < compressed.length(); i++){
+            chars[i] = compressed.charAt(i);
+        }
+        return compressed.length();
+    }
+}
