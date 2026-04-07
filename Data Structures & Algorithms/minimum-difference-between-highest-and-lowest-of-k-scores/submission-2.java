9b472fc28eaf64e81e01bae9bf47f1eadfb0e8c4
Keshav Maheshwari <90709089+kehav1534@users.noreply.github.com>
2026-04-07 10:03:32 +0530

    Add: minimum-difference-between-highest-and-lowest-of-k-scores - submission-2

---
+++ Data Structures & Algorithms/minimum-difference-between-highest-and-lowest-of-k-scores/submission-2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minimumDifference(int[] nums, int k) {
+        if(nums.length==1)return 0;
+        Arrays.sort(nums);
+        int minScore = nums[k-1] - nums[0];
+        //1 2 3
+        for(int i  = k; i < nums.length; i++){
+            int diff = nums[i] - nums[i-k+1];
+            minScore = diff < minScore? diff : minScore;
+        }
+        return minScore;
+    }
+}
