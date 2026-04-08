bde6f683b1c5bbb1f1d198e67b3c1e299532a21b
Keshav Maheshwari <90709089+kehav1534@users.noreply.github.com>
2026-04-09 00:34:01 +0530

    Add: minimum-size-subarray-sum - submission-8

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-8.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int sum = 0;
+        int left = 0;
+        int right = 0;
+        int count = 0;
+        int tempCount = 0;
+        while(right < nums.length){
+            if(sum+nums[right] < target){
+                sum += nums[right];
+                tempCount++;
+            }
+            else if(sum+nums[right]>=target){
+                tempCount++;
+                sum += nums[right];
+                while(left <= right && sum-nums[left] >= target){
+                    sum-=nums[left];
+                    tempCount--;
+                    left++;
+                }
+                count =( (tempCount < count || count==0) && tempCount>0)? tempCount : count;
+            }
+            right++;
+        }
+        return count;
+    }
+}
