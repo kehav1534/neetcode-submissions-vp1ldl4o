475e7ef50d9bade6b060938f8c62eb18f7065a46
Keshav Maheshwari <90709089+kehav1534@users.noreply.github.com>
2026-04-09 16:33:39 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices.length==1)return 0;
+        int left = 0;
+        int right = 1;
+        int profit = 0;
+
+        while(left < prices.length && right< prices.length){
+            if(prices[left] >= prices[right]){
+                left++;
+                right++;
+                continue;
+            }
+            while( right+1 < prices.length && prices[right] <= prices[right] && prices[left] >= prices[right]){
+                right++;
+            }
+            if(right < prices.length && prices[left] < prices[right]){
+                profit = profit+prices[right] - prices[left];
+                left++;
+                right++;
+                System.out.println("left :"+left+" right: "+right+ " profit: "+profit);
+
+            }
+        }
+        return profit;
+    }
+}
