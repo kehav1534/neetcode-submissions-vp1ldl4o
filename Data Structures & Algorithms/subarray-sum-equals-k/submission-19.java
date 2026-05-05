6d705b50eb67ce546848f883c56651d870c78a10
Keshav Maheshwari <90709089+kehav1534@users.noreply.github.com>
2026-05-05 14:58:37 +0530

    Add: subarray-sum-equals-k - submission-19

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-19.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        HashSet<Integer[]> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++){
+            int sum = 0;
+            List<Integer> arrList = new ArrayList<>();
+            for(int j = i; j < nums.length; j++){
+                sum+=nums[j];
+                arrList.add(nums[j]);
+                if(sum==k){
+                    Integer[] list = new Integer[arrList.size()];
+                    list = arrList.toArray(new Integer[0]);
+                    set.add(list);
+                }
+            }
+        }
+        return set.size();
+    }
+}
