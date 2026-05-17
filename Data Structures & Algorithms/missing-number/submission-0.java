af1b8dbae0b8b5facffc15cb3d243dd6cd9592fe
Keshav Maheshwari <90709089+kehav1534@users.noreply.github.com>
2026-05-17 13:08:43 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int total = 0;
+        for(int i = 0; i <= nums.length; i++){
+            total^=i;
+        }
+        int temp = 0;
+        for(int i:nums){
+            total^=i;
+        }
+        return total;
+    }
+}
