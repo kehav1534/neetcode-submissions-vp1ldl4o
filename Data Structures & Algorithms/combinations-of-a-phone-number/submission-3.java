090df2ae4b8b5dba41bde49617bff01360adf08a
Keshav Maheshwari <90709089+kehav1534@users.noreply.github.com>
2026-05-18 19:24:35 +0530

    Add: combinations-of-a-phone-number - submission-3

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-3.java
@@ -0,0 +1,31 @@
+class Solution {
+    Map<Character, String> combini = new HashMap<>();
+    public List<String> letterCombinations(String digits) {
+        List<String> list = new ArrayList<>();
+        if (digits.equals("")) return list;
+        combini.put('2', "abc");
+        combini.put('3', "def");
+        combini.put('4', "ghi");
+        combini.put('5', "jkl");
+        combini.put('6', "mno");
+        combini.put('7', "pqrs");
+        combini.put('8', "tuv");
+        combini.put('9', "wxyz");
+        recursive(list, digits, "", 0);
+        return list;
+        
+    }
+    public void recursive(List<String> list, String input, String word, int i){
+
+        if(i>=input.length()){
+            list.add(word);
+            return;
+        }
+        
+        String len = combini.get(input.charAt(i));
+        for(int j = 0; j < len.length(); j++){
+            recursive(list, input, word+len.charAt(j), i+1);
+        }
+        return;
+    }
+}
