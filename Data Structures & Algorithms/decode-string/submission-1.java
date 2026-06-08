75073486f7c7f18a1e1f6ba57d3e5a3ec46334c8
Keshav Maheshwari <90709089+kehav1534@users.noreply.github.com>
2026-06-08 23:38:35 +0530

    Add: decode-string - submission-1

---
+++ Data Structures & Algorithms/decode-string/submission-1.java
@@ -0,0 +1,32 @@
+class Solution {
+    int i = 0;
+    public String decodeString(String s) {
+        return decode(s);
+    }
+    String decode(String s){
+        String result = "";
+        int num = 0;
+        while(i<s.length()){
+            if(s.charAt(i)>='0' && s.charAt(i)<='9'){
+                num = num*10 + s.charAt(i)-'0';
+            }
+            else if(s.charAt(i)=='['){
+                i++;
+                String temp = decode(s);
+                result += multiple(temp, num);
+                num=0;
+            }
+            else if(s.charAt(i)==']') return result;
+            else{
+                result+=s.charAt(i);
+            }
+            i++;
+        }
+        return result;
+
+    }
+    String multiple(String res, int rep){
+        if(rep<=1) return res;
+        return res+multiple(res, rep-1);
+    }
+}
