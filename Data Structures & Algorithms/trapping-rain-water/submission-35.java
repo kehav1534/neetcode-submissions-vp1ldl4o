c583cb21f1ded75ac43f4f7f26191cb70692b3de
Keshav Maheshwari <90709089+kehav1534@users.noreply.github.com>
2026-06-13 02:23:41 +0530

    Add: trapping-rain-water - submission-35

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-35.java
@@ -0,0 +1,39 @@
+// Brute Force Approach..
+
+// Here, we're calculating left most Max (including current index)-> leftMaxArr
+// Here, we're calculating right most Max (including current index)-> rightMaxArr.
+// Now moving from sencd index -> i = 1, check which leftMaxArr[i] or rightMaxArr[i] is smaller, which tells 
+// maximum amount of water which can be filled.
+// This will allow calculating fillable water with respect to current height.
+
+
+class Solution {
+    public int trap(int[] height) {
+        int leftMax = 0;
+        int rightMax = 0;
+        int res = 0;
+        int[] leftMaxArr = new int[height.length];
+        int[] rightMaxArr = new int[height.length];
+
+        for(int i = 0; i < height.length; i++){
+            leftMax = Math.max(leftMax, height[i]);
+            rightMax = Math.max(rightMax, height[height.length-1-i]);
+            leftMaxArr[i] = leftMax;
+            rightMaxArr[height.length-1-i] = rightMax;
+        }
+        for(int i = 1; i < height.length-1; i++){
+            if(leftMaxArr[i] <= rightMaxArr[i]){
+                res+= Math.max( leftMaxArr[i]-height[i], 0);
+            }
+            else{
+                res+= Math.max( rightMaxArr[i]-height[i], 0);
+            }
+        }
+        return res;
+    }
+}
+
+
+
+
+
