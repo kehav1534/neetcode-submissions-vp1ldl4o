3ddefbc35a459236641a21e0d5fc62fddf61e025
Keshav Maheshwari <90709089+kehav1534@users.noreply.github.com>
2026-07-17 12:59:56 +0530

    Add: rotate-array - submission-6

---
+++ Data Structures & Algorithms/rotate-array/submission-6.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        Deque<Integer> q = new ArrayDeque<>();
+        for(int i = 0; i < nums.length; i++){
+            q.addLast(nums[i]);
+        }
+        for(int i = k; i > 0; i--){
+            q.addFirst(q.pollLast());
+        }
+        for(int i  = 0; i < nums.length; i++){
+            nums[i] = q.pollFirst();
+        }
+        q = null;
+    }
+}
